46bc8d0b6d303791785ee64a78ce736da949f607
Victor <victor_ganciar23@hotmail.com>
2018-07-11 16:39:39 -0300

    Subindo o repositorio para o github

---
+++ src/test/java/br/edu/ufabc/VaiDeBike/VaiDeBikeApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.ufabc.VaiDeBike;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaiDeBikeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
